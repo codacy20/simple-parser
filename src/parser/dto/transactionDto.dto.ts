4c774894e7a7389baa837df7dc3c6cfc320244ec
codacy20 <kmuc72@gmail.com>
2019-08-29 17:14:52 +0200

    dto was corrected

--- src/parser/dto/transactionDto.dto.ts
+++ src/parser/dto/transactionDto.dto.ts
@@ -3,11 +3,16 @@ enum Mutation {
   deduction,
 }
 
+interface IMutation {
+  operation: Mutation;
+  amount: number;
+}
+
 export class TransactionDto {
   readonly transactionReference: number;
   readonly accountNumber: string;
   readonly startBalance: number;
   readonly endBalance: number;
   readonly description: string;
-  readonly mutation: Mutation;
+  readonly mutation: IMutation;
 }
